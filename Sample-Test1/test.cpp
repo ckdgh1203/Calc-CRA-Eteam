35e57fdd447ab798d0a2ab1df9d4c3e5b78edc92
YullKim <yull618@naver.com>
2024-05-20 17:06:30 +0900

    update minus test

--- Sample-Test1/test.cpp
+++ Sample-Test1/test.cpp
@@ -4,3 +4,11 @@ TEST(TestCaseName, TestName) {
   EXPECT_EQ(1, 1);
   EXPECT_TRUE(true);
 }
+
+TEST(calc, getMinusTest) {
+	calc calc;
+
+	EXPECT_EQ(calc.getMinus(0, 0), 0);
+	EXPECT_EQ(calc.getMinus(2, 1), 1);
+	EXPECT_EQ(calc.getMinus(4, 2), 2);
+}
