1d29e51300332b532ab5a96d5cbfb8e363ddd05f
KimYoonjae <rladbswo1124@gmail.com>
2024-05-20 17:04:15 +0900

    Update getSum

--- Sample-Test1/test.cpp
+++ Sample-Test1/test.cpp
@@ -10,3 +10,13 @@ TEST(TestCaseName, GetSumTest) {
 	Calc calc;
 	EXPECT_EQ(10, calc.getSum(7, 3));
 }
+
+TEST(TestCaseName, GetSumTest2) {
+	Calc calc;
+	EXPECT_EQ(15, calc.getSum(17, -2));
+}
+
+TEST(TestCaseName, GetSumTest3) {
+	Calc calc;
+	EXPECT_EQ(-5, calc.getSum(-1, -4));
+}
