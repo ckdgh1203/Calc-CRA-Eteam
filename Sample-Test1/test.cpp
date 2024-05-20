cbab68854e92ca6ef6e9d6b5521a7fd862454245
kicheolha <kcha2299@gmail.com>
2024-05-20 17:09:50 +0900

    Update Zegop

--- Sample-Test1/test.cpp
+++ Sample-Test1/test.cpp
@@ -1,6 +1,21 @@
-#include "pch.h"
+﻿#include "pch.h"
+#include "../calc/calc.cpp"
 
-TEST(TestCaseName, TestName) {
-  EXPECT_EQ(1, 1);
-  EXPECT_TRUE(true);
+using namespace std;
+
+TEST(CalcTest, ZegopException) {
+	Calc calc;
+	EXPECT_THROW(calc.getZegop(46341), invalid_argument);
+}
+
+TEST(CalcTest, ZegopResultForZero) {
+	Calc calc;
+	EXPECT_EQ(calc.getZegop(0), 0);
+}
+
+TEST(CalcTest, ZegopResultForNormalInput) {
+	Calc calc;
+	int input = 777;
+	int expected = (input) * (input);
+	EXPECT_EQ(calc.getZegop(input), expected);
 }
--- calc/calc.cpp
+++ calc/calc.cpp
@@ -1,4 +1,14 @@
+﻿#include <stdexcept>
+#include <iostream>
+using namespace std;
+
 class Calc {
 public:
+	int getZegop(int a) {
+		if (a > 46340) {
+			throw invalid_argument("제곱의 결과가 최대치를 벗어납니다.");
+		}
 
+		return (a * a);
+	}
 };
