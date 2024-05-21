b203f131642d5c06c62d183b585b0f47f6189e1d
SeungjunHa <gkak1121@naver.com>
2024-05-21 09:27:56 +0900

    Implement Test Case

--- Sample-Test1/test.cpp
+++ Sample-Test1/test.cpp
@@ -1,6 +1,33 @@
-#include "pch.h"
+﻿#include "pch.h"
+#include "../calc/calc.cpp"
+
+class CalcFixture : public testing::Test {
+public:
+	Calc calc;
+private:
+};
 
 TEST(TestCaseName, TestName) {
   EXPECT_EQ(1, 1);
   EXPECT_TRUE(true);
+}
+
+TEST_F(CalcFixture, Positive) {
+	EXPECT_EQ(calc.getGop(2, 5), 10);
+}
+
+TEST_F(CalcFixture, FirstNegative) {
+	EXPECT_EQ(calc.getGop(-2, 5), -10);
+}
+
+TEST_F(CalcFixture, SecondNegative) {
+	EXPECT_EQ(calc.getGop(2, -5), -10);
+}
+
+TEST_F(CalcFixture, BothNegative) {
+	EXPECT_EQ(calc.getGop(-2, -5), 10);
+}
+
+TEST_F(CalcFixture, Zero) {
+	EXPECT_EQ(calc.getGop(2, 0), 0);
 }
